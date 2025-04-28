f2c7c4120e553004d39368f4bec2a00e705e48b1
ChristiaanBoe <Christiaan.Boerkamp@live.nl>
2025-04-28 23:39:21 +0200

    Create pelicanconf.py

---
+++ docs/pelicanconf.py
@@ -0,0 +1,10 @@
+# docs/pelicanconf.py
+AUTHOR = 'Christiaan Boerkamp'
+SITENAME = 'TINA Documentation'
+SITEURL = ''
+
+PATH = 'content'
+TIMEZONE = 'Europe/Amsterdam'
+DEFAULT_LANG = 'en'
+
+THEME = 'notmyidea'  # or any Pelican theme you like
