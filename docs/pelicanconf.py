ce7a471aee79e2bee35c66a2fdbec85dd2fbca6b
ChristiaanBoe <Christiaan.Boerkamp@live.nl>
2025-04-29 16:16:38 +0200

    Update pelicanconf.py

--- docs/pelicanconf.py
+++ docs/pelicanconf.py
@@ -2,8 +2,14 @@
 AUTHOR = 'Christiaan Boerkamp'
 SITENAME = 'TINA Documentation'
 SITEURL = ''
-
+# Where to find your content
 PATH = 'content'
+
+# treat _every_ .md/.rst in content/ as a page
+PAGE_PATHS    = ['.']
+
+# (optional) disable “articles” entirely
+ARTICLE_PATHS = []
 TIMEZONE = 'Europe/Amsterdam'
 DEFAULT_LANG = 'en'
 
